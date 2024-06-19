821012097355c9475f81580203bb4ada3c50425d
Luis Kendall <108600731+luiskendall@users.noreply.github.com>
2024-06-19 03:00:42 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,40 @@
-const http = require('http');
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+// Use the body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Read the comments from the file
+var comments = JSON.parse(fs.readFileSync('comments.json', 'utf8'));
+
+// Create a route for the comments
+app.get('/comments', function (req, res) {
+  res.json(comments);
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+// Create a route to add a new comment
+app.post('/comments', function (req, res) {
+  var comment = req.body;
+  comments.push(comment);
+  fs.writeFileSync('comments.json', JSON.stringify(comments));
+  res.json(comment);
+});
+
+// Create a route to delete a comment
+app.delete('/comments/:id', function (req, res) {
+  var id = req.params.id;
+  comments = comments.filter(function (comment) {
+    return comment.id !== id;
+  });
+  fs.writeFileSync('comments.json', JSON.stringify(comments));
+  res.json({status: 'ok'});
+});
+
+// Start the server
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
 });
